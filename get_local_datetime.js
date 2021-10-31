b7cad7cabed6baa37d504ac0ec3904acfbfdc47d
Christian <christian@simplglobal.com>
2021-10-31 14:39:23 -0700

    Fixed HTML date/time issue

--- get_local_datetime.js
+++ get_local_datetime.js
@@ -3,7 +3,10 @@ window.addEventListener('load', () => {
 	const now = new Date();
 	now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
 	now.setMilliseconds(null);
-	console.log(now.toISOString().slice(0,-8))
-	document.getElementById('now').value =
-		now.toISOString().slice(0, -8);
+	console.log(now.toISOString().slice(0, 10));
+	console.log(now.toISOString().slice(11, 16));
+	const datestr = now.toISOString().slice(0, 10);
+	const timestr = now.toISOString().slice(11, 16);
+	document.getElementById('nowDate').value = datestr;
+	document.getElementById('nowTime').value = timestr;
 });
